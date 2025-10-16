340d06698de11bf279ba7448d24e04291d84a37a
ramta chhipa <ramtachhipa.cse27@jecrc.ac.in>
2025-10-16 23:10:52 +0530

    updated

--- chapter4.py
+++ chapter4.py
@@ -4,3 +4,53 @@ tup = (87,64,33,95,76);
 print(tup)
 
 tup1 = ()
+<<<<<<< HEAD
+=======
+print(type(tup))
+tup2 = (1,)
+print(type(tup2))
+
+print(tup.index(64))
+print(tup.count(64))
+
+#WAP to ask the enter name of their 3 favourate movie name
+# movies = []
+# mov1= input("enter 1st favourate movie:")
+# mov2= input("enter 2nd favourate movie:")
+# mov3= input("enter 3rd favourate movie:")
+
+# print(movies)
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+
+# print(movies)
+
+
+
+#wap to check whetehr a list contain pellindrome or not
+# list1 = [1,2,1]
+# list2 = [1,2,3]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("palindrome")
+# else:
+#     print("not a pellindrome")    
+
+
+
+#wap to store the values as a list and sort them (c,d,a,a,b,b,a)
+# grade = ["C","D","A","A","C","D"]
+# grade.sort()
+# print(grade)
+
+
+#Dictionary and set
+
+
+>>>>>>> 72d780b (update)
